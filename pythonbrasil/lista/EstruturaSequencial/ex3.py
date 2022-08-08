f91619d861901d09a8c85c31f4fe4b9ed6008db5
juniper lee <95495037+farseers@users.noreply.github.com>
2022-08-08 16:04:04 -0300

    Add files via upload

---
+++ pythonbrasil/lista/EstruturaSequencial/ex3.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+n1	=	float(input('Digite um primeiro número:'))
+n2	=	float(input('Digite um segundo número:'))
+n=[n1,n2]
+s=sum(n)
+print(f"A soma é {s}")
---
+++ pythonbrasil/lista/EstruturaSequencial/ex4.py
@@ -0,0 +1,10 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+n1	=	float(input('Digite a primeira nota:'))
+n2	=	float(input('Digite a segunda nota:'))
+n3	=	float(input('Digite a terceira nota:'))
+n4	=	float(input('Digite a quarta nota:'))
+n=[n1,n2,n3,n4]
+n=sum(n)
+s=n/4
+print(f"A média é {s}")
