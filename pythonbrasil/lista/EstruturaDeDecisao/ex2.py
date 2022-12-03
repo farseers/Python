4270744a3fbf63dbf94c0ce7846ff72c67e2da9c
poppywar <juliakcc03@gmai.com>
2022-12-03 00:13:45 -0300

    TESTE

--- pythonbrasil/lista/EstruturaDeDecisao/ex2.py
+++ pythonbrasil/lista/EstruturaDeDecisao/ex2.py
@@ -1,6 +1,6 @@
 #Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
 
-n1=float(input("Digite um número qualquer:"))
+n1=float(input("Digite um número:"))
 if n1>0:
     print ("O número é positivo")
 elif n1<0:
